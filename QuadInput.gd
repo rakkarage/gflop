a5063a7fdc96f107fe03bc789792aabfdd049bff
Rakka Rage <rakkarage@gmail.com>
2024-10-04 23:53:57 -0400

    simplify

--- QuadInput.gd
+++ QuadInput.gd
@@ -16,33 +16,27 @@ func _ready() -> void:
 	_area.mouse_entered.connect(func() -> void: _is_mouse_inside = true)
 	_area.mouse_exited.connect(func() -> void: _is_mouse_inside = false)
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton or event is InputEventMouseMotion or event is InputEventScreenDrag or event is InputEventScreenTouch:
-		return
-	_viewport.push_input(event)
-
 func _on_input_event(_camera: Camera3D, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if _use_input_mask and not is_mouse_inside_mask:
+		return
+	if not _is_mouse_inside:
 		return
 
 	var mesh_size: Vector2 = mesh.size
 	var event_pos3D := global_transform.affine_inverse() * event_position
-	var event_pos2D := Vector2()
-	var current_time := Time.get_ticks_msec() / 1000.0
+	var event_pos2D := Vector2(event_pos3D.x, -event_pos3D.y) / mesh_size + Vector2(0.5, 0.5)
+	event_pos2D *= Vector2(_viewport.size.x, _viewport.size.y)
 
-	if _is_mouse_inside:
-		event_pos2D = Vector2(event_pos3D.x, -event_pos3D.y) / mesh_size + Vector2(0.5, 0.5)
-		event_pos2D *= Vector2(_viewport.size.x, _viewport.size.y)
-	elif _last_event_pos2D != null:
-		event_pos2D = _last_event_pos2D
+	var current_time := Time.get_ticks_msec() / 1000.0
 
 	event.position = event_pos2D
 	if event is InputEventMouse:
 		event.global_position = event_pos2D
 
 	if event is InputEventMouseMotion or event is InputEventScreenDrag:
-		event.relative = event_pos2D - (_last_event_pos2D if _last_event_pos2D != null else event_pos2D)
-		event.velocity = event.relative / (current_time - _last_event_time)
+		event.relative = event_pos2D - _last_event_pos2D
+		var time_diff := current_time - _last_event_time
+		event.velocity = event.relative / time_diff if time_diff > 0 else Vector2.ZERO
 
 	_last_event_pos2D = event_pos2D
 	_last_event_time = current_time
